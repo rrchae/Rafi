6ab8b0f5e778de909608524965c1b1b2ce7b5bf6
rrchae <rrchae146@gmail.com>
2021-12-09 22:02:49 -0800

    task is done

---
+++ src/test/java/Push.java
@@ -0,0 +1,25 @@
+
+public class Push {
+	
+	
+
+		
+		
+		public void add() {
+			
+			System.out.println("id is 6758");
+		}
+		
+		public static void main(String[] args) {
+			
+			
+			Push p = new Push();
+			p.add();
+			
+		}
+		
+		
+		
+	}
+
+
